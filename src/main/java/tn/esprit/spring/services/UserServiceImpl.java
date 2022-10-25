de5325f5b511c58b430c28f229a4d66acd0d85f6
Sofiene mokaddem <73641797+sofieneMoka@users.noreply.github.com>
2022-10-25 13:18:27 +0100

    Add Crypt Password

--- src/main/java/tn/esprit/spring/services/UserServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/UserServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import tn.esprit.spring.model.User;
@@ -14,9 +16,14 @@ public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	UserRepository userRepository ;
+	
+	PasswordEncoder passwordEncoder;
 
 	@Override
 	public void addUser(User user) {
+		this.passwordEncoder = new BCryptPasswordEncoder();
+		user.setEmailVerified(0);
+		user.setPassword((this.passwordEncoder.encode(user.getPassword())));
         userRepository.save(user);	
 	}
 
